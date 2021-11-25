6c3bd147995f5ec2db01d01979acd4dbd2549324
sv6-UCC <65443362+sv6-UCC@users.noreply.github.com>
2021-11-25 16:40:51 +0000

    Add files via upload

--- AlarmTest.py
+++ AlarmTest.py
@@ -1,31 +1,48 @@
 from time import gmtime, strftime
 import time
 import datetime
-from alarm import year, month, day, hour, minute, second
+#from alarm import year, month, day, hour, minute, second
+import alarm
 
-format = "d-%m-%Y"
 
 class AlarmTest():
     def __init__(self, time, date):
         self.time = time
         self.date = date
 
-    def test_date(date):
+    def test_date(self):
         isValidDate = True
         try:
-            datetime.datetime.strptime(date, format)  #1st test: tests that the date format is correct 
-            datetime.datetime(int(year.get()), int(month.get()), int(day.get()))  #2nd test: tests that the selected date is correct  
-        except ValueError:
-            isValidDate = False
+            day =self.date[0]
+            month=self.date[1]
+            year=self.date[2]
+            datetime.datetime(day, month, year)
+            return "valid date"
+            #datetime.datetime(self.date)
+            #datetime.datetime.strptime(date, format)  #1st test: tests that the date format is correct
+              #2nd test: tests that the selected date is correct
 
-    def test_time(time): 
-        string_comparison = "test"
-        try: 
-            if (type(time) == type(string_comparison)):  #check time type
-                if (0 < hour.get() < 24) and (0 < minute.get() < 60) and (0 < second.get() < 60):  #check that the time is valid 
-                    return "valid time"
+        except ValueError:
+            return "invalid date"
+
+    def test_time(self):
+       # string_comparison = "test"
+        try:
+            hour =self.time[0]
+            minute=self.time[1]
+            second=self.time[2]
+            if hour<0 or hour>23 or minute <0 or minute>59 or second<0 or second>59:
+                return "invalid time"
+            #if (type(self.time) == type(string_comparison)):  #check time type
+            if (0 < hour < 24) and (0 < minute < 60) and (0 < second < 60):  #check that the time is valid
+                return "valid time"
         except TypeError:
             return "invalid time"
+
+#my_test =AlarmTest([8,22,50],[2021,12,22])
+my_test =AlarmTest([8,22,50],[2021,2,30])
+print(my_test.test_time())
+print(my_test.test_date())
 
 
 
